5363e599438d76f1d8cc40643f2b064d94d32879
Endre Moen <endremoen@hotmail.com>
2026-01-19 13:15:39 +0100

    shell scripts deleted
    1. check_monitor.sh - Monitoring verification script
    2. code_writer_auto.sh - Automated code writer script
    3. debug_state.sh - Debug state checking
    4. monitor_debug.sh - Debug monitor script
    5. monitor_polling.sh - Alternative polling-based monitor
    6. monitor_polling_test.sh - Polling monitor test
    7. monitor_simple.sh - Simple monitor variant
    8. run_code_writer.sh - Code writer runner
    9. run_system.sh - System runner
    10. show_code_writer.sh - Display code writer status
    11. verify_monitor.sh - Monitor verification

--- agent_system/show_code_writer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Search for main_loop:"
-grep -n "main_loop" /home/endrem/prosjekt/nanocode/code_writer.sh
-echo ""
-echo "Around line 260:"
-sed -n '258,270p' /home/endrem/prosjekt/nanocode/code_writer.sh
